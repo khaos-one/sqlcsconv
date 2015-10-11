d109bd2b17f3a2ea10a4e52a6116f3cfd1fb3f19
Егор Зеленский <i@khaos.su>
2015-10-11 13:28:17 +0300

    Initial commit

---
+++ Options.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace sqlcsconv {
+    class Options {
+        public string DbHost { get; set; }
+        public string DbUser { get; set; }
+        public string DbPassword { get; set; }
+        public string Target { get; set; }
+        
+        public string SourceEncoding { get; set; }
+        public string DestEncoding { get; set; }
+        
+        public bool GenerateScript { get; set; }
+
+        public bool Verbose { get; set; }
+
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage() {
+            return HelpText.AutoBuild(this, (HelpText c) => HelpText.DefaultParsingErrorsHandler(this, c));
+        }
+    }
+}
